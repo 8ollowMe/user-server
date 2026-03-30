9b91247b855a3e3bc5e7012a9d3361e208408c25
qldo <idzhdl@gmail.com>
2026-03-30 14:37:21 +0900

    style: Annotation 삭제

--- src/main/java/com/followme/userserver/application/mapper/UserMapper.java
+++ src/main/java/com/followme/userserver/application/mapper/UserMapper.java
@@ -1,7 +1,7 @@
 package com.followme.userserver.application.mapper;
 
 import com.followme.userserver.application.dto.UserRegisterRequestDto;
-import com.followme.userserver.application.dto.UserResponseDto; // 💡 DTO import 추가
+import com.followme.userserver.application.dto.UserResponseDto;
 import com.followme.userserver.domain.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
