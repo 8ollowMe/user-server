3c60930497cdefdbb8af9586e83a7eab814cdf0d
qldo <idzhdl@gmail.com>
2026-03-30 14:32:55 +0900

    fix: ResponseDto 에 userId 추가

--- src/main/java/com/followme/userserver/application/mapper/UserMapper.java
+++ src/main/java/com/followme/userserver/application/mapper/UserMapper.java
@@ -1,7 +1,7 @@
 package com.followme.userserver.application.mapper;
 
 import com.followme.userserver.application.dto.UserRegisterRequestDto;
-import com.followme.userserver.application.dto.UserResponseDto;
+import com.followme.userserver.application.dto.UserResponseDto; // 💡 DTO import 추가
 import com.followme.userserver.domain.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,5 +13,7 @@ public interface UserMapper {
     @Mapping(target = "status", constant = "PENDING")
     User toEntity(UserRegisterRequestDto request);
 
+    @Mapping(source = "id", target = "userId")
     UserResponseDto toResponseDto(User user);
+    
 }
