c091c6c4be9385e02e191a4d02fa4336462dd90e
qldo <idzhdl@gmail.com>
2026-04-07 12:19:22 +0900

    refactor: SwaggerConfig 로 이름 변경 및 수정

---
+++ src/main/java/com/followme/userserver/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.followme.userserver.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Collections.singletonList(securityRequirement))
+                .info(new Info()
+                        .title("User Service API")
+                        .version("v1.0.0")
+                        .description("사용자 및 배송 담당자 관리 API 명세서"));
+    }
+}
